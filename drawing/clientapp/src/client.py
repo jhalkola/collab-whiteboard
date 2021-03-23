cef131a4d8422c9ff75ad0523d4ff22b9e36af37
Jskallio <32382026+Jskallio@users.noreply.github.com>
2021-03-23 15:49:53 +0200

    Optimizing

--- drawing/clientapp/src/client.py
+++ drawing/clientapp/src/client.py
@@ -13,14 +13,23 @@ import random
 
 
 def unpack_helper(fmt, data):
+    '''
+    This function unpacks only part of the data and returns the variables
+    '''
     size = struct.calcsize(fmt)
     return struct.unpack(fmt, data[:size])
 
 def unpack_helper_part(fmt, data):
+    '''
+    This function unpacks only part of the data and returns the variables AND the leftover bytes
+    '''
     size = struct.calcsize(fmt)
     return struct.unpack(fmt, data[:size]), data[size:]
 
 def exit_handler():
+    '''
+    This function handles quiting the program. Closes the files and sends disconnect message
+    '''
     global pub
     global name
     global client_disc_topic
@@ -29,166 +38,205 @@ def exit_handler():
     message = struct.pack("8s16s", client_disc_topic, name.encode('utf-8'))
     pub.send(message)
 
-def send_pixel(x, y, name):
-    global pub
-    global client_draw_topic
-    
 
-connect_message_format = "8s16s"
-connect_message_topic = b'cnt_mesg'
-client_draw_topic = b'drw_updt'
-image_update_topic = b'img_updt'
-client_disc_topic = b'clt_disc'
-
-
-server_to_client_port = 8000
-client_to_server_port = 8001
-context = zmq.Context()
-
-users = {}
-own_color = ""
-image_as_string = ""
-
-is_automatic = os.environ.get('TESTING')
-if is_automatic=="true":
-    name = "client{}".format(random.randint(0,500))
-else:
-    name = os.environ.get('NAME')
-server = "tcp://serverd"#os.environ.get('SERVER_CONNECT_URI')
-atexit.register(exit_handler)
-while True:
-    #print("Give your name:")
-    
-    #print(name)
-    name = "{}                ".format(name)[:16]
-    while True:
-        try:
-            connect_socket = context.socket(zmq.REQ)
-            connect_socket.connect("{}:8002".format(server))
-            break
-        except Exception as e:
-            print(e)
-            time.sleep(1)
-    print("Connected")
-    connect_message = struct.pack("16s", name.encode('utf-8'))
-    connect_socket.send(connect_message)
-    answer = connect_socket.recv()
-    connect_socket.close()
-    success = unpack_helper("i", answer)[0]
-    if int(success) == 1:
-        user_count = int(unpack_helper("ii", answer)[1])
-        unpacked, left = unpack_helper_part("ii7s7500s",answer)
-        for i in range(user_count-1):
-            user, left = unpack_helper_part("16s7s", left)
-            user_name = user[0].decode('utf-8')
-            user_color = user[1].decode('utf-8')
-            users[user_name] = user_color
-        own_color = unpacked[2].decode('utf-8')
-        users[name] = own_color
-        image_as_string = unpacked[3]
-        break
-    else:
-        print("This name is already in use")
-        quit()
-        pass
-
-
-
-sub = context.socket(zmq.SUB)
-sub.connect("{}:{}".format(server, server_to_client_port))
-sub.setsockopt(zmq.SUBSCRIBE, image_update_topic)
-
-pub = context.socket(zmq.PUB)
-pub.connect("{}:{}".format(server, client_to_server_port))
-pygame.init()
-
-scale = 10
-display_width = 50*scale
-display_height = 50*scale
-
-gameDisplay = pygame.display.set_mode((display_width+200, display_height))
-pygame.display.set_caption("Client")
-
-image = pygame.image.fromstring(image_as_string, (50, 50), "RGB")
-
-
-def scale_image(image, size):
-    image = pygame.transform.scale(image, size)
-    return image
 
 def update_namelist(users):
+    '''
+    Updates game display to display the users received from the server
+    '''
     divider = 5
     for user in users:
         text = font.render(user, False, users[user])
         gameDisplay.blit(text, (display_width+10, divider))
         divider += 25
 
-clock = pygame.time.Clock()
+
+
+#Format for the connect message
+connect_message_format = "8s16s"
+#Set topics for different messages for publishing and subscribing
+connect_message_topic = b'cnt_mesg'
+client_draw_topic = b'drw_updt'
+image_update_topic = b'img_updt'
+client_disc_topic = b'clt_disc'
+
+#Start the ZeroMQ context and store ports for the connection
+server_to_client_port = 8000
+client_to_server_port = 8001
+context = zmq.Context()
+
+#Initialize variables
+users = {}
+own_color = ""
+image_as_string = ""
+
+#If TESTING-env variable has been set then use randomized names. Otherwise use env-var NAME as name
+is_automatic = os.environ.get('TESTING')
+if is_automatic=="true":
+    name = "client{}".format(random.randint(0,50000))
+else:
+    name = os.environ.get('NAME')
+#Format the name to be 16 characters long
+name = "{}                ".format(name)[:16]
+#Get the server ip
+server = os.environ.get('SERVER_CONNECT_URI')
+
+
+#Try connecting to the connect server
+while True:
+    try:
+        connect_socket = context.socket(zmq.REQ)
+        connect_socket.connect("{}:8002".format(server))
+        break
+    except Exception as e:
+        print(e)
+        time.sleep(1)
+
+#Format the connection message and send it to the server
+connect_message = struct.pack("16s", name.encode('utf-8'))
+connect_socket.send(connect_message)
+
+#Wait for the answer from the server and close the connection
+answer = connect_socket.recv()
+connect_socket.close()
+
+#Check if connection was succesfull
+success = unpack_helper("i", answer)[0]
+
+
+if int(success) == 1:
+    #If connection is succesfull then get users, clients own color and the image from the server
+    user_count = int(unpack_helper("ii", answer)[1])
+    unpacked, left = unpack_helper_part("ii7s300s",answer)
+    for i in range(user_count-1):
+        user, left = unpack_helper_part("16s7s", left)
+        user_name = user[0].decode('utf-8')
+        user_color = user[1].decode('utf-8')
+        users[user_name] = user_color
+    own_color = unpacked[2].decode('utf-8')
+    users[name] = own_color
+    image_as_string = unpacked[3]
+else:
+    #If not succesfull then quit the program
+    print("This name is already in use")
+    quit()
+
+
+#connect the subscriber and publisher
+sub = context.socket(zmq.SUB)
+sub.connect("{}:{}".format(server, server_to_client_port))
+sub.setsockopt(zmq.SUBSCRIBE, image_update_topic)
+
+pub = context.socket(zmq.PUB)
+pub.connect("{}:{}".format(server, client_to_server_port))
+
+
+#Assign the exit handler
+atexit.register(exit_handler)
+
+#Initialize the pygame and screen
+pygame.init()
+
+scale = 50
+display_width = 10*scale
+display_height = 10*scale
+
+gameDisplay = pygame.display.set_mode((display_width+200, display_height))
+pygame.display.set_caption("Client")
+
+#Get the image from the data received from the server
+image = pygame.image.fromstring(image_as_string, (10, 10), "RGB")
+
+
+
+#Checker for if the client should stop
 done = False
+
+#Variables for checking if we should draw new pixel
 mouseIsPressed = False
 mouseWasPressed = False
 mouse_prev_x = -1
 mouse_prev_y = -1
-color = (255,0,0)
+
+#Font for the name texts
 font = pygame.font.Font('freesansbold.ttf', 20)
 
 
-
+#Open the log file
 now = datetime.now()
 date_string = now.strftime("%d-%m-%Y_%H:%M:%S")
-log_file = open("log/log_{}_{}.txt".format(name, date_string),'w')
+log_file = open("log/log_{}_{}.txt".format(date_string, name),'w')
 
+#Variables for checking the time for automated drawing
 last_time = time.time()
 send_time = 0
 
-scaled = scale_image(image, (display_width, display_height))
+#Scale the image and draw screen for the first time
+scaled = pygame.transform.scale(image, (display_width, display_height))
 gameDisplay.fill((255,255,255))
 pygame.draw.rect(gameDisplay, (0, 0, 0), (display_width,0,2,display_height))
 update_namelist(users)
 gameDisplay.blit(scaled,(0, 0))
 pygame.display.flip()
-print("Im alive")
+
+#Start working
 while not done:
     try:
+        #Try receiving data on subscribers
         data = sub.recv(zmq.NOBLOCK)
-        
+
+        #Get the topic of the message
         topic = unpack_helper("8s", data)[0]
+
         if topic == image_update_topic:
-            msg, left = unpack_helper_part("8si7500s16s", data)
+            #Topic is image update
+            #unpack user count, image and the name of editer 
+            msg, left = unpack_helper_part("8si300s16s", data)
             user_count = msg[1]
             users = {}
             for i in range(user_count):
+                #Unpack the user names and colors from the rest of the data
                 user, left = unpack_helper_part("16s7s", left)
                 user_name = user[0].decode('utf-8')
                 user_color = user[1].decode('utf-8')
                 users[user_name] = user_color
+            
+            #If the editor is us then log the time of drawing
             editor = msg[3].decode('utf-8')
             if editor == name and is_automatic=="true":
                 time_taken = time.time()-send_time
                 log_file.write("{}\n".format(time_taken))
                 send_time=0
-            img = pygame.image.fromstring(msg[2],(50,50), "RGB")
+                #Assing random time for the next automatic pixel
+                last_time = time.time()+random.random()*5
+            
+            #Get the image data from the bytes
+            img = pygame.image.fromstring(msg[2],(10,10), "RGB")
             image.blit(img, (0, 0))
-            scaled = scale_image(image, (display_width, display_height))
+            #Scale and display the image
+            scaled = pygame.transform.scale(image, (display_width, display_height))
             gameDisplay.fill((255,255,255))
             pygame.draw.rect(gameDisplay, (0, 0, 0), (display_width,0,2,display_height))
             update_namelist(users)
             gameDisplay.blit(scaled,(0, 0))
             pygame.display.flip()
     except Exception as e:
-        pass#print(e)
-
+        pass
+    #Check pygame events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.MOUSEBUTTONDOWN:
+            #Mouse down event
             if event.button == 1:
                 mouseIsPressed = True
         if event.type == pygame.MOUSEBUTTONUP:
+            #Mouse up event
             if event.button == 1:
                 mouseIsPressed = False
             
     if mouseIsPressed:
+        #If mouse was pressed down at this moment or if we moved from one pixel to another then send new pixel to server
         x, y = pygame.mouse.get_pos()
         x = math.floor(x/scale)
         y = math.floor(y/scale)
@@ -202,10 +250,10 @@ while not done:
         mouseWasPressed = False
     
     if is_automatic=="true":
-        if time.time()>last_time+1 and send_time==0:
-            x = random.randint(0,50)
-            y = random.randint(0,50)
-            last_time = time.time()
+        #If testing mode is on send random pixels to the server
+        if time.time()>last_time and send_time==0:
+            x = random.randint(0,10)
+            y = random.randint(0,10)
             send_time = time.time()
             message = struct.pack("8s16sii", client_draw_topic, name.encode('utf-8'), x, y)
             pub.send(message)
--- drawing/serverapp/src/server.py
+++ drawing/serverapp/src/server.py
@@ -1,73 +1,107 @@
 
 import zmq
 import struct
-from time import sleep
+from time import sleep, time
 import pygame
 import os
 import json
 
-address = os.environ.get("SERVER_LISTEN_URI")
+
 
 def get_colors():
+    '''
+    This function gets the color data from the json file
+    '''
     with open("colors.json") as file:
         data = json.load(file)
     return data.values()
 
 def unpack_helper(fmt, data):
+    '''
+    Unpack helper for structs
+    '''
     size = struct.calcsize(fmt)
     return struct.unpack(fmt, data[:size])
 
+
+
 def main():
+    #Get all the colors as list
     all_colors = list(get_colors())
-    connect_message_format = "8s16s"
+
+    #Zeromq pub and sub topics
     client_draw_topic = b'drw_updt'
     image_update_topic = b'img_updt'
     client_disc_topic = b'clt_disc'
 
+    #Ports for the communication
     server_to_client_port = 8000
     client_to_server_port = 8001
 
+    #Start the context and start publisher and subscribers
     context = zmq.Context()
+
     sub = context.socket(zmq.SUB)
     sub.bind("tcp://*:{}".format(client_to_server_port))
     sub.setsockopt(zmq.SUBSCRIBE, client_draw_topic)
     sub.setsockopt(zmq.SUBSCRIBE, image_update_topic)
     sub.setsockopt(zmq.SUBSCRIBE, client_disc_topic)
+
     pub = context.socket(zmq.PUB)
     pub.bind("tcp://*:{}".format(server_to_client_port))
 
+    #Start the connection server
     connect_socket = context.socket(zmq.REP)
     connect_socket.bind("tcp://*:8002")
 
+    #Initialize the users dictionary
     users = {}
+
+    #Timer for backing up the image
+    last_save_time = time()
+    backup_time = 30
+
+    #Try getting the image from existing file. If this fails then create new image
     try:
         image = pygame.image.load_basic("log/canvas.bmp")
     except:
-        image = pygame.Surface((50,50))
+        image = pygame.Surface((10,10))
         image.fill((255,255,255))
 
+    #Start serving
     while True:
         try:
+            #Check for subscriber messages
             data = sub.recv(zmq.NOBLOCK)
+
+            #Get the topic of the message
             topic = unpack_helper("8s", data)[0]
+
             if topic == client_draw_topic:
+                #If topic is client draw topic then receive the pixel coordinates and color and do the drawing into the image
                 _, user, x, y = struct.unpack("8s16sii", data)
                 color = users[user.decode('utf-8')]
-                #print("Draw from user: {} {}".format(user, users[user.decode('utf-8')]))
                 x = int(x)
                 y = int(y)
                 color = pygame.Color(color)
+
+                #Use pygame to draw pixel
                 pygame.draw.rect(image, color, (x, y, 1, 1))
-                pygame.image.save(image, "log/canvas.bmp")
+
+                #Initialize the response message and send it to everyone
                 userdata = ""
                 original_user = user
                 for user in users:
                     userdata += user + users[user]
                 imagedata = pygame.image.tostring(image, "RGB")
 
-                response = struct.pack("8si7500s16s{}s".format(len(userdata)), image_update_topic, len(users), imagedata, original_user, userdata.encode('utf-8'))
+                response = struct.pack("8si300s16s{}s".format(len(userdata)), image_update_topic, len(users), imagedata, original_user, userdata.encode('utf-8'))
                 pub.send(response)
+
+                
+                
             if topic == client_disc_topic:
+                #If the topic is client disconnecting topic then remove the client from the users list. This will be updated to the clients in the next draw message
                 _, user = struct.unpack("8s16s", data)
                 del(users[user.decode('utf-8')])
                 print("User left: {}".format(user.decode('utf-8')))
@@ -77,16 +111,20 @@ def main():
             pass
             
         try:
+            #Check if we got new clients connecting
             data = connect_socket.recv(zmq.NOBLOCK)
+            #Get the name of connecting user
             new_name = struct.unpack("16s", data)[0].decode('utf-8')
             
+            #If the name is already taken then respond with error message. Otherwise add user to list and send them their own color and current image
             response = b'0'
             if new_name in users:
                 response = struct.pack("i",0)
                 print("Already user with this name")
             else:
                 print("Adding user: {}".format(new_name))
-                form = "ii7s7500s{}s".format(len(users)*23)
+                #Initialize response to the connection message
+                form = "ii7s300s{}s".format(len(users)*23)
                 color = ""
                 for c in all_colors:
                     if not c in users.values():
@@ -94,18 +132,29 @@ def main():
                         break
                 users[new_name] = color
                 colordata = color.encode('utf-8')
+
+                #get all the users and their colors into string
                 userdata = ""
                 
                 for user in users:
                     userdata += user + users[user]
+                
+                #Get the byte data from the image
                 imagedata = pygame.image.tostring(image, "RGB")
-                
+
                 response = struct.pack(form, 1, len(users), colordata, imagedata, userdata.encode('utf-8'))
-                
+
+            #Send back the response                
             connect_socket.send(response)
                 
         except Exception as e:
             #print(e)
             pass
+    
+
+    if time()>last_save_time+backup_time:
+        #Backup the image
+        pygame.image.save(image, "log/canvas.bmp")
+        last_save_time = time()
 
 main()
